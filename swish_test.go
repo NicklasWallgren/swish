dd572f8efc65fdd7b9aa3982df48a2214703b701
Nicklas Wallgren <nicklas.wallgren@gmail.com>
2020-01-02 11:44:49 +0100

    Updated unit test

--- swish_test.go
+++ swish_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestPaymentRequest(t *testing.T) {
-
 	configuration := NewConfiguration(&TestEnvironment, GetResourcePath("certificates/test.pem"), GetResourcePath("certificates/test.key"))
 	payload := PaymentPayload{PayeePaymentReference: "0123456789", CallbackUrl: "https://myfakehost.se/swishcallback.cfm", PayeeAlias: "9871065216", PayerAlias: "1231181189", Amount: "100", Currency: "SEK"}
 
@@ -20,18 +19,9 @@ func TestPaymentRequest(t *testing.T) {
 
 		return
 	}
-
-	fmt.Println(response)
-	fmt.Println(response.Id)
-	fmt.Println(response.Token)
-	fmt.Println(response.Url)
-
 	response2, err := swish.PaymentResult(context.Background(), response.Id)
 
 	if err != nil {
-		fmt.Println("here here")
-		fmt.Println(err)
-
 		return
 	}
 
